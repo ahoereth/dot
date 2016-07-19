0c0f5b3a42ec3a2e2ded541489f6d7f39488ab9e
Alexander Höreth <a.hoereth@gmail.com>
2016-07-19 17:00:58 +0200

    Move uosvpn to its own executable script after changes to jupiter-vpn-py.

---
+++ shell/bin/uosvpn.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo python2 ~/shell/juniper-vpn-py/juniper-vpn.py \
+    --host vpn-e.serv.uos.de \
+    --realm uos \
+    --stdin DSID=%DSID% \
+    openconnect \
+        --juniper %HOST% \
+        --cookie-on-stdin
