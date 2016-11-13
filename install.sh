1533fa2400231cdde92ad29283688ac0c9fc43de
Alexander Höreth <ahoereth@gmail.com>
2016-11-13 14:14:44 +0100

    Add some more install commands and add .gitconfig

--- install.sh
+++ install.sh
@@ -3,3 +3,13 @@ DOT=~/dot
 
 ln -s $DOT/.zshrc ~/.zshrc
 ln -s $DOT/.zprofile ~/.zprofile
+
+cp $DOT/.gitconfig ~/.gitconfig
+
+pacman -S base-devel nodejs npm haskell-parsec
+
+pip install -r $DOT/requirements.txt
+
+yarn global add diff-so-fancy
+
+(cd compleat && ./Setup.lhs configure && ./Setup.lhs build && sudo ./Setup.lhs install)
