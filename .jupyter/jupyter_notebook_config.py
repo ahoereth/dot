b82512eb9a89e6e2b0eb72ec19e9d038b5528914
Alexander Höreth <ahoereth@gmail.com>
2017-05-26 19:21:05 +0200

    Add jupyter configuration.

---
+++ .jupyter/jupyter_notebook_config.py
@@ -0,0 +1,37 @@
+"""Jupyter notebook python configuration."""
+
+
+def pre_save_scrub_output(model, path, contents_manager):
+    """Scrub output before saving notebooks.
+
+    As seen on:
+    github.com/jupyter/notebook/blob/d751f1/docs/source/extending/savehooks.rst
+    """
+    if model['type'] != 'notebook':  # Only run on notebooks.
+        return
+    if model['content']['nbformat'] != 4:  # Only run on nbformat v4.
+        return
+    for cell in model['content']['cells']:  # Clear each cell.
+        if cell['cell_type'] != 'code':
+            continue
+        cell['outputs'] = []
+        cell['execution_count'] = None
+
+
+def pre_save(model, path, contents_manager):
+    """Run functions before saving a file."""
+    funcs = [pre_save_scrub_output]
+    for func in funcs:
+        func(model=model, path=path, contents_manager=contents_manager)
+
+
+def post_save(model, os_path, contents_manager):
+    """Run functions after saving a file."""
+    funcs = []
+    for func in funcs:
+        func(model=model, os_path=os_path, contents_manager=contents_manager)
+
+
+# pylint:disable=E0602
+c.FileContentsManager.pre_save_hook = pre_save  # type: ignore
+c.FileContentsManager.post_save_hook = post_save  # type: ignore
--- install.sh
+++ install.sh
@@ -24,6 +24,11 @@ yaourt -Sy `cat dependencies-yaourt.txt`
 sudo pip3 install -r $DOT/dependencies-pip3.txt
 # sudo pip2 install -r $DOT/py2-requirements.txt  # None currently
 
+# Setup jupyter configuration
+mkdir -p ~/.jupyter/nbconfig
+ln -s $DOT/.jupyter/jupyter_notebook_config.py ~/.jupyter/jupyter_notebook_config.py
+ln -s $DOT/.jupyter/nbconfig/notebook.json ~/.jupyter/nbconfig/notebook.json
+
 # Install visual studio code extensions
 for x in $(cat dependencies-vsc.txt); do code --install-extension $x; done
 
